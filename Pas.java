a5dcb5a239c375046476ba6b324f009b6c791d95
Manikandan81480 <mani81480@gmail.com>
2016-10-16 05:00:12 -0700

    Update and rename README.md to Pas.java

---
+++ Pas.java
@@ -0,0 +1,8 @@
+class Pas
+{
+public static void main(String... args)
+{
+Scanner x=new Scanner(System.in);
+int a=x.nextInt();
+if(a>0):System.out.print("pasitive"):System.out.println("negative");
+}}
